d3aa56fa9b04dfa86a6e297439e1e27e1a49ab25
Edvinas Jonusas <ed.jonusas@gmail.com>
2021-08-04 20:54:48 +0300

    feat: navigation lang, position, logo animation

---
+++ components/global/SVG/FacebookIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="25" cy="25" r="24.5" stroke="white" />
+    <g clip-path="url(#clip0)">
+      <path
+        d="M22.4835 35V25.6154H20V22.2365H22.4835V19.3505C22.4835 17.0826 23.9505 15 27.3308 15C28.6994 15 29.7114 15.1311 29.7114 15.1311L29.6317 18.2864C29.6317 18.2864 28.5996 18.2764 27.4733 18.2764C26.2543 18.2764 26.059 18.8377 26.059 19.7693V22.2365H29.7286L29.5689 25.6154H26.059V35H22.4835Z"
+        fill="white"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0">
+        <rect width="9.7286" height="20" fill="white" transform="translate(20 15)" />
+      </clipPath>
+    </defs>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'FacebookIcon',
+}
+</script>
