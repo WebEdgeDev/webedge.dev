a7114fa38c33ed57962e20122705cbf19692bb8f
artik777 <arturpojarkov91@gmail.com>
2021-08-04 12:47:26 +0300

    feat: work section

--- components/global/Home/ContactSection.vue
+++ components/global/Home/ContactSection.vue
@@ -1,3 +1,8 @@
 <template>
-  <div>Hero</div>
+  <section class="bg-white text-main">
+    <h2 class="uppercase font-sans text-[47px] relative z-30">
+      Contact
+      <span class="font-coda text-[44px]">Us</span>
+    </h2>
+  </section>
 </template>
